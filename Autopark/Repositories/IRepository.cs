8278eb2694a04f4c9465cb4d4efa3346ad9179fa
Dima Kotov <damir.tarasov2016@gmail.com>
2021-11-20 21:47:11 +0300

    Создание интерфейса IRepository и класса Repository

---
+++ Autopark/Repositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Autopark.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> Get();
+        IEnumerable<T> Get(Func<T, bool> predicate);
+        T FindById(int id);
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ Autopark/Repositories/Repository.cs
@@ -0,0 +1,70 @@
+﻿using Autopark.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Autopark.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly AutoparkContext context;
+        private readonly DbSet<T> dbSet;
+
+        public Repository(AutoparkContext _context)
+        {
+            context = _context;
+            dbSet = context.Set<T>();
+        }
+
+        public IEnumerable<T> Get()
+        {
+            return dbSet.AsNoTracking().ToList();
+        }
+
+        public IEnumerable<T> Get(Func<T, bool> predicate)
+        {
+            return dbSet.AsNoTracking().Where(predicate).ToList();
+        }
+
+        public IEnumerable<T> GetWithInclude(params Expression<Func<T, object>>[] includeProperties)
+        {
+            return Include(includeProperties).ToList();
+        }
+
+        public IEnumerable<T> GetWithInclude(Func<T, bool> predicate, params Expression<Func<T, object>>[] includeProperties)
+        {
+            return Include(includeProperties).Where(predicate).ToList();
+        }
+
+        private IQueryable<T> Include(params Expression<Func<T, object>>[] includeProperties)
+        {
+            IQueryable<T> query = dbSet.AsNoTracking();
+            return includeProperties.Aggregate(query, (current, includeProperty) => current.Include(includeProperty));
+        }
+
+        public T FindById(int id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public void Insert(T entity)
+        {
+            dbSet.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            context.Entry(entity).State = EntityState.Modified;
+            context.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            dbSet.Remove(entity);
+            context.SaveChanges();
+        }
+    }
+}
