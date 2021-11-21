3abfec49b17055ffb333bfd038313fea96ff4d4a
Dima Kotov <damir.tarasov2016@gmail.com>
2021-11-21 11:50:53 +0300

    Добавлена команда CloseAppCommand

--- Autopark/ViewModels/MainWindowViewModel.cs
+++ Autopark/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,37 @@
-﻿using Autopark.ViewModels.Base;
+﻿using Autopark.Infrastructure.Commands;
+using Autopark.ViewModels.Base;
+using System.Windows;
+using System.Windows.Input;
 
 namespace Autopark.ViewModels
 {
     class MainWindowViewModel : BaseViewModel
     {
-        
+        #region Команды
+
+        #region CloseAppCommand
+
+        public ICommand CloseAppCommand { get; }
+
+        private bool CanCloseAppCommandExecute(object o) => true;
+
+        private void OnCloseAppCommandExecuted(object o)
+        {
+            Application.Current.Shutdown();
+        }
+
+        #endregion
+
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            #region Комманды
+
+            CloseAppCommand = new ActionCommand(OnCloseAppCommandExecuted, CanCloseAppCommandExecute);
+
+            #endregion
+
+        }
     }
 }
