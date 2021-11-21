5725e95e95084219a099d590ba71ca107c380f47
Dima Kotov <damir.tarasov2016@gmail.com>
2021-11-21 12:27:51 +0300

    Добавлены команды MinimizeAppCommand и MaximizeAppCommand

--- Autopark/ViewModels/MainWindowViewModel.cs
+++ Autopark/ViewModels/MainWindowViewModel.cs
@@ -9,16 +9,34 @@ namespace Autopark.ViewModels
     {
         #region Команды
 
+        #region MinimizeAppCommand
+
+        public ICommand MinimizeAppCommand { get; }
+
+        private bool CanMinimizeAppCommandExecute(object o) => true;
+
+        private void OnMinimizeAppCommandExecuted(object o) => Application.Current.MainWindow.WindowState = WindowState.Minimized;
+
+        #endregion
+
+        #region MaximizeAppCommand
+
+        public ICommand MaximizeAppCommand { get; }
+
+        private bool CanMaximizeAppCommandExecute(object o) => true;
+
+        private void OnMaximizeAppCommandExecuted(object o) => Application.Current.MainWindow.WindowState = 
+            Application.Current.MainWindow.WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+
+        #endregion
+
         #region CloseAppCommand
 
         public ICommand CloseAppCommand { get; }
 
         private bool CanCloseAppCommandExecute(object o) => true;
 
-        private void OnCloseAppCommandExecuted(object o)
-        {
-            Application.Current.Shutdown();
-        }
+        private void OnCloseAppCommandExecuted(object o) => Application.Current.Shutdown();
 
         #endregion
 
@@ -28,10 +46,11 @@ namespace Autopark.ViewModels
         {
             #region Комманды
 
+            MinimizeAppCommand = new ActionCommand(OnMinimizeAppCommandExecuted, CanMinimizeAppCommandExecute);
+            MaximizeAppCommand = new ActionCommand(OnMaximizeAppCommandExecuted, CanMaximizeAppCommandExecute);
             CloseAppCommand = new ActionCommand(OnCloseAppCommandExecuted, CanCloseAppCommandExecute);
 
             #endregion
-
         }
     }
 }
