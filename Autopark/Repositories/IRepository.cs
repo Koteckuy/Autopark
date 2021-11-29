28a37e52d9b06be282c7d2dbf6df2642084adccd
Dima Kotov <damir.tarasov2016@gmail.com>
2021-11-29 13:52:12 +0300

    Редактура Repository

--- Autopark/Repositories/IRepository.cs
+++ Autopark/Repositories/IRepository.cs
@@ -1,12 +1,14 @@
-﻿using System;
+﻿using Autopark.Models.Entities;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Autopark.Repositories
 {
-    public interface IRepository<T> where T : class
+    public interface IRepository<T> where T : class, IEntity, new()
     {
-        IEnumerable<T> Get();
-        IEnumerable<T> Get(Func<T, bool> predicate);
+        IQueryable<T> Get();
         T FindById(int id);
         void Insert(T entity);
         void Update(T entity);
--- Autopark/Repositories/Repository.cs
+++ Autopark/Repositories/Repository.cs
@@ -1,70 +1,56 @@
 ﻿using Autopark.Models;
+using Autopark.Models.Entities;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 
+
 namespace Autopark.Repositories
 {
-    public class Repository<T> : IRepository<T> where T : class
+    public class Repository<T> : IRepository<T> where T : class, IEntity, new()
     {
         private readonly AutoparkContext context;
-        private readonly DbSet<T> dbSet;
 
         public Repository(AutoparkContext _context)
         {
             context = _context;
-            dbSet = context.Set<T>();
         }
 
-        public IEnumerable<T> Get()
+        public IQueryable<T> Get()
         {
-            return dbSet.AsNoTracking().ToList();
-        }
-
-        public IEnumerable<T> Get(Func<T, bool> predicate)
-        {
-            return dbSet.AsNoTracking().Where(predicate).ToList();
-        }
-
-        public IEnumerable<T> GetWithInclude(params Expression<Func<T, object>>[] includeProperties)
-        {
-            return Include(includeProperties).ToList();
-        }
-
-        public IEnumerable<T> GetWithInclude(Func<T, bool> predicate, params Expression<Func<T, object>>[] includeProperties)
-        {
-            return Include(includeProperties).Where(predicate).ToList();
-        }
-
-        private IQueryable<T> Include(params Expression<Func<T, object>>[] includeProperties)
-        {
-            IQueryable<T> query = dbSet.AsNoTracking();
-            return includeProperties.Aggregate(query, (current, includeProperty) => current.Include(includeProperty));
+            return context.Set<T>();
         }
 
         public T FindById(int id)
         {
-            return dbSet.Find(id);
+            return Get().FirstOrDefault(x => x.ID == id);
         }
 
         public void Insert(T entity)
         {
-            dbSet.Add(entity);
+            context.Set<T>().Add(entity);
             context.SaveChanges();
         }
 
         public void Update(T entity)
         {
-            context.Entry(entity).State = EntityState.Modified;
+            context.Entry(FindById(entity.ID)).CurrentValues.SetValues(entity);
+            context.Entry(FindById(entity.ID)).State = EntityState.Modified;
             context.SaveChanges();
         }
 
         public void Delete(T entity)
         {
-            dbSet.Remove(entity);
-            context.SaveChanges();
+            T dbEntity = FindById(entity.ID);
+            if (dbEntity != null)
+            {
+                entity.Deleted = true;
+                context.Entry(FindById(entity.ID)).CurrentValues.SetValues(entity);
+                context.Entry(FindById(entity.ID)).State = EntityState.Modified;
+                context.SaveChanges();
+            }
         }
     }
 }
